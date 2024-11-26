8757af3d8400655b0a65274e13facbe562ee25b4
catediana <catherinenanyala@gmail.com>
2024-11-26 10:19:35 -0800

    created a directory then initialised the repository and then added the three files

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+# 1. Add a View Function for the Home page.
+@app.route("/")
+def homepage():
+    return render_template("home.html")
+
+# 2. Add a View Function for the About page.
+@app.route("/about")
+def about():
+    return render_template("about.html")
+     
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
